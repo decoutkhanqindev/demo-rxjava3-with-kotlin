66c0f2454ef116506ce6473748745c0363baa108
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-27 21:10:46 +0700

    concat in rxjava3

--- src/main/kotlin/data_types/creating/creating_05.kt
+++ src/main/kotlin/data_types/creating/creating_05.kt
@@ -4,13 +4,15 @@ import io.reactivex.rxjava3.core.Observable
 import java.util.concurrent.TimeUnit
 
 fun main() {
-  val observable1: Observable<String> = Observable.range(2, 9) // 9 - 2 + 1 values
+  val observable1: Observable<String> =
+    Observable.range(2, 9) // 9 - 2 + 1 values
     .map { "[1] $it" }
     .doOnSubscribe {
       println("Subscribe to observable 1.....${Thread.currentThread()}")
     }
 
-  val observable2: Observable<String> = Observable.interval(1, TimeUnit.SECONDS)
+  val observable2: Observable<String> =
+    Observable.interval(1, TimeUnit.SECONDS)
     .take(3)
     .map { "[2] $it" }
     .doOnSubscribe {
