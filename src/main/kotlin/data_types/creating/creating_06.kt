b1b70abfc3e633c320eeae95ca2ae8b99d8b9f1b
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-27 21:14:29 +0700

    zip in rxjava3

---
+++ src/main/kotlin/data_types/creating/creating_06.kt
@@ -0,0 +1,28 @@
+package org.example.data_types.creating
+
+import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.TimeUnit
+
+fun main() {
+  val observable1: Observable<String> =
+    Observable.range(2, 9) // 9 - 2 + 1 values
+    .map { "[1] $it" }
+    .doOnSubscribe {
+      println("Subscribe to observable 1.....${Thread.currentThread()}")
+    }
+
+  val observable2: Observable<String> =
+    Observable.interval(1, TimeUnit.SECONDS)
+    .take(3)
+    .map { "[2] $it" }
+    .doOnSubscribe {
+      println("Subscribe to observable 2.....${Thread.currentThread()}")
+    }
+
+  // zip will aggregate 2 observables together into 1 common observable and it follows the index
+  val zipped: Observable<String> =
+    Observable.zip(observable1, observable2, { t1, t2 -> "$t1, $t2" })
+  zipped.materialize().subscribe(::println)
+
+  Thread.sleep(10000)
+}
