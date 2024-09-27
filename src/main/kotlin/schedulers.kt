99ee1aa2b339fec3c41c257e104d58855c65a6c0
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-27 15:57:44 +0700

    schedulers in rxjava3

--- src/main/kotlin/schedulers.kt
+++ src/main/kotlin/schedulers.kt
@@ -1,25 +1,26 @@
 package org.example
 
+import io.reactivex.rxjava3.core.Notification
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 fun main() {
   val disposable: Disposable = Observable.just(1, 2, 3)
-    .doOnEach {
+    .doOnEach { it: Notification<Int> ->
       if (it.isOnNext) {
         println("doOnEach1 onNext ${it.value} and on thread ${Thread.currentThread().name}")
       }
     }
     .subscribeOn(Schedulers.io())
     .observeOn(Schedulers.computation())
-    .doOnEach {
+    .doOnEach { it: Notification<Int> ->
       if (it.isOnNext) {
         println("doOnEach2 onNext ${it.value} and on thread ${Thread.currentThread().name}")
       }
     }
     .map { it * 2 }
-    .doOnEach {
+    .doOnEach { it: Notification<Int> ->
       if (it.isOnNext) {
         println("doOnEach3 onNext ${it.value} and on thread ${Thread.currentThread().name}")
       }
@@ -27,7 +28,7 @@ fun main() {
     .subscribeOn(Schedulers.single())
     .observeOn(Schedulers.io())
     .filter { it % 2 == 0 }
-    .doOnEach {
+    .doOnEach { it: Notification<Int> ->
       if (it.isOnNext) {
         println("doOnEach4 onNext ${it.value} and on thread ${Thread.currentThread().name}")
       }
