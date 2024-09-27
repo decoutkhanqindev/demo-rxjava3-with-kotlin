d3fcb48a78db45ab735ecba576688106d5a55c3c
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-27 20:37:37 +0700

    defer in rxjava3

---
+++ src/main/kotlin/data_types/creating/creating_03.kt
@@ -0,0 +1,28 @@
+package org.example.data_types.creating
+
+import io.reactivex.rxjava3.core.Notification
+import io.reactivex.rxjava3.core.Observable
+import kotlin.Int
+import kotlin.random.Random
+
+fun main() {
+  val coldObservable: Observable<Int> = Observable.defer {
+    println("Call lambda outer.... ${Thread.currentThread()}")
+    Observable.fromCallable {
+      println("Call lambda inner.... ${Thread.currentThread()}")
+      Thread.sleep(1000)
+      Random.nextInt()
+    }
+  }
+
+  println("Before subscribe.... ${Thread.currentThread()}")
+  Thread.sleep(1000);
+
+  repeat(3) { index: Int ->
+    coldObservable.materialize().subscribe { value: Notification<Int> ->
+      println("Subscriber ${index + 1}: $value")
+    }
+    Thread.sleep(1000)
+    printSeparator()
+  }
+}
