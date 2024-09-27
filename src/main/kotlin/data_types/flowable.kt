634d0dfd466d6500418420f6e92e3b114dfec7f6
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-27 17:22:04 +0700

    flowable in rxjava3

--- src/main/kotlin/data_types/flowable.kt
+++ src/main/kotlin/data_types/flowable.kt
@@ -7,9 +7,9 @@ import org.reactivestreams.Subscription
 fun main() {
   Flowable.fromIterable(0..10000)
     // If the observer cannot handle data in time
-    .onBackpressureLatest() // only handle latest value
-    .onBackpressureBuffer() // value will contain in buffer
-    .onBackpressureDrop() // value will drop
+    .onBackpressureLatest() // only handle latest data
+    .onBackpressureBuffer() // data will contain in buffer
+    .onBackpressureDrop() // data will drop
     .subscribe(object : FlowableSubscriber<Int> {
       lateinit var subscription: Subscription // help observer can handle send request to observable
 
