daf552b60ee3365c6019e3d9e0f3326f20b4b491
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-27 20:53:56 +0700

    timer, interval in rxjava3

---
+++ src/main/kotlin/data_types/creating/creating_04.kt
@@ -0,0 +1,29 @@
+package org.example.data_types.creating
+
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.Disposable
+import java.util.concurrent.TimeUnit
+
+fun main() {
+  println("Start timer.....")
+  // timer will delay n seconds and push 1 value is 0L and complete
+  Observable.timer(/* delay = */ 5, /* unit = */ TimeUnit.SECONDS)
+    .materialize()
+    .subscribe(::println)
+
+  Thread.sleep(10000)
+  printSeparator()
+
+  println("Start interval.....")
+  // interval will delay every n seconds and push 1 value and repeat
+  val disposable: Disposable =
+    Observable.interval(/* initialDelay = */ 2, /* period = */ 3, /* unit = */ TimeUnit.SECONDS)
+      .materialize()
+      .subscribe(::println)
+
+  Thread.sleep(30000)
+  disposable.dispose()
+  printSeparator()
+
+  // Schedulers.computation() is default Schedulers in timer and interval
+}
