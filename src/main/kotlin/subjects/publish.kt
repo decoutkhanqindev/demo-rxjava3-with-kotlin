406b4ae59ff66ba28e9fcf0bb6947550515649ab
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-27 22:59:21 +0700

    publish subject in rxjava3

---
+++ src/main/kotlin/subjects/publish.kt
@@ -0,0 +1,49 @@
+package org.example.subjects
+
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.subjects.PublishSubject
+
+fun main() {
+  val subject: PublishSubject<Int?> = PublishSubject.create<Int>()
+  // no observers can receive these values
+  subject.onNext(1)
+  subject.onNext(2)
+  subject.onNext(3)
+
+  Thread.sleep(1000)
+
+  val disposable1: Disposable = subject.subscribe { v: Int? -> println("Observer 1 received: $v") }
+  // only observer 1 can receive these values
+  subject.onNext(4)
+  subject.onNext(5)
+  subject.onNext(6)
+
+  Thread.sleep(1000)
+
+  val disposable2: Disposable = subject.subscribe { v: Int? -> println("Observer 2 received: $v") }
+  // observer 1 and observer 2 can receive these values
+  subject.onNext(7)
+  subject.onNext(8)
+  subject.onNext(9)
+  // observer 2 is disposed
+  disposable2.dispose()
+
+  Thread.sleep(1000)
+
+  // only observer 1 can receive these values
+  subject.onNext(10)
+  subject.onNext(11)
+  subject.onNext(12)
+
+  Thread.sleep(1000)
+
+  // observer 1 is disposed
+  disposable1.dispose()
+
+  Thread.sleep(1000)
+
+  // no observers can receive these values
+  subject.onNext(13)
+  subject.onNext(14)
+  subject.onNext(15)
+}
